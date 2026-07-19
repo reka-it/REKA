96a9cebfc0f87e8b190d417bf8c7de61c93eda58
specter <sigurd.thornes@gmail.com>
2026-07-19 17:40:28 +0200

    quicky card update

--- app/components/Card/Card.stories.tsx
+++ app/components/Card/Card.stories.tsx
@@ -13,8 +13,28 @@ type Story = StoryObj<typeof Card>;
 
 export const Default: Story = {
 	args: {
-		children: "UwU",
+		children: "This is a card wow",
 		style: { width: "200px", height: "200px" },
+		bgtype: "normal"
+	},
+	render: (args) => <Card {...args} />,
+};
+
+export const Pattern: Story = {
+	args: {
+		children: "This is a card wow",
+		style: { width: "200px", height: "200px" },
+		bgtype: "pattern",
+	},
+	render: (args) => <Card {...args} />,
+};
+
+export const Image: Story = {
+	args: {
+		children: "This is a card wow",
+		style: { width: "200px", height: "200px" },
+		bgtype: "image",
+		src: "/public/map.png",
 	},
 	render: (args) => <Card {...args} />,
 };
--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,18 +1,29 @@
+import Pattern from "../Pattern/Pattern";
 import styles from "./Card.module.scss";
 
 type CardProps = {
 	children?: React.ReactNode;
 	className?: string;
 	style?: React.CSSProperties;
+	bgtype?: "normal" | "pattern" | "image";
+	src?: string;
 };
 
 export default function Card({
 	children,
 	className = "",
 	style,
+	bgtype = "normal",
+	src,
 }: CardProps) {
 	return (
 		<div className={`${styles.card} ${className}`} style={style}>
+			{bgtype === "pattern" &&
+				<Pattern smallest={7} largest={7} spacing={20} opacity={0.3} color="pink" offsetY={-5} offsetX={-5} />
+			}
+			{bgtype === "image" &&
+				<img src={src} className={styles.image} />
+			}
 			{children}
 		</div>
 	);
