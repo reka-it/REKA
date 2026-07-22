5675d8dd0d928bb78dd7946e5f2b009d7b2f6d48
specter <sigurd.thornes@gmail.com>
2026-07-22 19:41:07 +0200

    imposible firework stuff

--- app/components/Firework/Firework.stories.tsx
+++ app/components/Firework/Firework.stories.tsx
@@ -1,15 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Firework from "./Firework";
+import Button from "../Button/Button";
+import { useState } from "storybook/internal/preview-api";
 
 export default {
 	title: "Components/Firework",
 	component: Firework,
 	parameters: { layout: "centered" },
-	args: {},
 } satisfies Meta<typeof Firework>;
 
 type Story = StoryObj<typeof Firework>;
 
 export const Default: Story = {
-	render: (args) => <Firework {...args} />,
+	render: (_) => {
+		const [active, setActive] = useState(false);
+		return (
+			<>
+				<Button onClick={() => setActive(true)}> Trigger </Button>
+				<Firework active={active} setActive={setActive} />
+			</>
+		)
+	}
 };
--- app/components/Firework/Firework.tsx
+++ app/components/Firework/Firework.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useMemo } from "react";
 import styles from "./Firework.module.scss";
+import { createPortal } from "react-dom";
 
 type FireworkProps = {
+	active: boolean;
+	setActive: (v: boolean) => void;
 };
 
-export default function Firework({ }: FireworkProps) {
-	return (
-		<div className={styles.firework}></div>
-	);
+function randInt(range: number): number {
+	return Math.floor(Math.random() * range);
+}
+
+export default function Firework({ active, setActive }: FireworkProps) {
+	const length = 30;
+
+	const gradients = useMemo<string>(() => {
+		if (!active) return "";
+		return Array.from({ length }).map((_) => (`radial-gradient(circle, red 10px, #0000 0)`)).join(",")
+	}, [active])
+
+	const positions = useMemo<string>(() => {
+		if (!active) return "";
+		return Array.from({ length }).map((_) => (`${randInt(100)}% ${randInt(100)}%`)).join(",")
+	}, [active])
+
+	useEffect(() => {
+		if (!active) return;
+
+		setTimeout(() => {
+			setActive(false)
+		}, 2000)
+	}, [active])
+
+	return createPortal(<div
+		className={active ? styles.firework : ""}
+		style={{ backgroundImage: gradients, backgroundPosition: positions }}
+	/>,
+		document.body
+	)
 }
